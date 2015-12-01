83afcb515e9a2b068d8abfa1d29513f4a14ed3af
hschoenburg <hschoenburg@gmail.com>
2015-11-30 17:10:07 -0800

    11-30 version

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,17 @@
 $(function(){
 
-	 setInterval(memberCount, 1000);
+	setInterval(memberCount, 3000);
 	var $signupBtn = $('#signupBtn');
 
 	// hide all pages except for the login page
 	// $('.page').hide();
-	$('#loginPage').show();
+	$('#login-page').show();
 	$('#navElements').hide();
 
 	// Page one signup button takes user to membership signup page
 
 	$('#signupBtn').on('click', function(){
-		$('#loginPage').hide();
+		$('#login-page').hide();
 		$('#memberSignup').show();
 	});
 
@@ -19,120 +19,30 @@ $(function(){
 	$('#submitBtn').on('click', function(e){
 		e.preventDefault;
 		$('#memberSignup').hide();
-		$('#loginPage').show();
+		$('#login-page').show();
 	})
 
-	// Page One login function takes you to workout/recovery selection page
-	$('#loginBtn').on('click', function(){
-		$('#navElements').show();
-		$('#loginPage').hide();
-		$('#optionsPage').show();
-	});
-
-	// Page Two workout/recovery page
-
-	// Select workout button that takes you to workout selection page (cardio, sports, strength)
-	$('#workoutBtn').on('click', function(){
-		$('#optionsPage').hide();
-		$('#workoutPage').show();
-	});
-
-	// Workout selection page
-
-	// Cardio button takes you to cardio challenge page
-	$('#cardioBtn').on('click', function(){
-		$('#workoutPage').hide();
-		$('#cardioPage').show();
-	});
-
-	// Sports button takes you to sports challenge page
-	$('#sportBtn').on('click', function(){
-		$('#workoutPage').hide();
-		$('#sportPage').show();
-	});
-
-	// Stength button takes you to strength challenge page
-	$('#strengthBtn').on('click', function(){
-		$('#workoutPage').hide();
-		$('#strengthPage').show();
-	});
-
-	// Once you finish a challenge, click the done button to log workout and checkout D3 visualization
-	$('.doneBtn').on('click', function(){
-		$('.page').hide();
-		$('#visualPage').show();
-	});
-
-	// Recovery selection pages
-
-	// Selecting recovery takes you to the set goals / log relapse page
-	$('#recoveryBtn').on('click', function(){
-		$('#optionsPage').hide();
-		$('#recoveryPage').show();
-	});
-
-	// Set goals button allows you to set a recovery goal (week, month, year, etc)
-	$('#setGoalBtn').on('click', function(){
-		$('#recoveryPage').hide();
-		$('#setGoalsPage').show();
-	});
-
-	// Lapse button allows you to log relapse occurrence (date, time, feelings, emotions, etc.)
-	$('#lapseBtn').on('click', function(){
-		$('#recoveryPage').hide();
-		$('#logLapsePage').show();
+	$('#loginBtn').on('click', function(e){
+		var username = $('#username').val();
+		localStorage.setItem('username', username);
 	})
 
-	// Go button takes you back to workout / recovery selection page
-	$('.goBtn').on('click', function(){
-		$('.page').hide();
-		$('#optionsPage').show();
-	})
+	var name = localStorage.getItem('username');
+	$('#member-name').text(name);
 
-	// Navbar functionality
 
-	// Home icon takes you to login page
-	$('#navHome').on('click', function(e){
-		$('.page').hide();
-		$('#navElements').hide();
-		$('#loginPage').show();
-	});
 
-	// Heart icon takes you to cardio challenge page
-	$('#navCardio').on('click', function(){
-		$('.page').hide();
-		$('#cardioPage').show();
-	});
 
-	// Flag icon takes you to sports challenge page
-	$('#navSports').on('click', function(){
-		$('.page').hide();
-		$('#sportPage').show();
-	});
+	
 
-	// Tower icon takes you to strength challenge page
-	$('#navStrength').on('click', function(){
-		$('.page').hide();
-		$('#strengthPage').show();
-	});
-
-	// Lightning icon takes you to log relapse page
-	$('#navRelapse').on('click', function(){
-		$('.page').hide();
-		$('#logLapsePage').show();
-	});
-
-	// User icon takes you to edit user info page 
-	$('#navUser').on('click', function(){
-		console.log('edit user info page');
-	})
+	
 	
 });
 
 // Random member count generator
 function memberCount(){
-	var $count = $('#num');
-	var random = Math.floor(Math.random() * 1000);
+	var $count = $('.num');
+	var random = Math.floor(Math.random() * 100);
 
 	var currentCount = Number(removeCommas($count.text()));
 	currentCount += random;
